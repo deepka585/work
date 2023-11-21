76af8f7b8787bad4a3095d6cf8fd806b4fac0986
deepka585 <151506414+deepka585@users.noreply.github.com>
2023-11-21 09:28:50 +0300

    Add files via upload

---
+++ ConsoleЭсси20.11№2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Rus");
+    int day;
+    cout << "Введите номер дня недели:"; cin >> day;
+    if ((day == 1) || (day == 2) || (day == 3) || (day == 4))
+        cout << "В этот день 4 пары." << endl;
+    else if (day == 5)
+        cout << "В этот день 3 пары." << endl;
+    else if ((day == 6) || (day == 7))
+        cout << "В этот день нет пар." << endl;
+    system("pause");
+    return 0;
+}
