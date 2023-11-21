c37a208f09daa09401d31edcce2bd6c41b14d7aa
deepka585 <151506414+deepka585@users.noreply.github.com>
2023-11-21 09:32:31 +0300

    Add files via upload

---
+++ ConsoleЭсси-Эзинг.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char ch;
+    short int a, b, c;
+    cout << "enter the number" << endl;
+    cin >> a;
+    cout << "enter second number" << endl;
+    cin >> b;
+    cout << "result" << endl;
+    cin >> c;
+    if (c == a * b) {
+        cout << "right";
+    }
+    else cout << "Wrong.Correct answer >> " << a * b;
+    return 0;
+
+}
---
+++ ConsoleЭсси20.11№3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int masiv[11];
+	masiv[0] = 0;
+	masiv[1] = 1;
+	masiv[2] = 2;
+	masiv[3] = 3;
+	masiv[4] = 4;
+	masiv[5] = 5;
+	masiv[6] = 6;
+	masiv[7] = 7;
+	masiv[8] = 8;
+	masiv[9] = 9;
+	masiv[10] = 10;
+	int sum = 0;
+	for (int i = 0; i <= 10; i++) {
+		if (i % 2 != 0) {
+			sum += i;
+		}
+	}
+	cout << "Сумма нечётных чисел >>" << sum << endl;
+}
